13a577991f6aeb26edaf01d86d7aae7eac953f39
David Walker <david@skibro.com>
2023-05-30 18:46:04 +0100

    clean up main server file

--- src/server/main.ts
+++ src/server/main.ts
@@ -4,7 +4,8 @@ import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes";
 import connectDB from "./config/db";
 
-dotenv.config();
+dotenv.config({ debug: true });
+const PORT = process.env.PORT || 3000;
 
 connectDB();
 
@@ -12,10 +13,12 @@ const app = Express();
 
 app.use("/api/products", productRoutes);
 
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + React + TypeScript!");
-});
+// app.get("/hello", (_, res) => {
+//   res.send(PORT);
+// });
 
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000...")
+ViteExpress.listen(app, PORT as number, () =>
+  console.log(
+    `Server is listening in ${process.env.NODE_ENV} mode on port ${PORT}...`
+  )
 );
