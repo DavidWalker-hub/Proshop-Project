f58237b72b47005a8beacd5ba15137fd8e7277f4
David Walker <david@skibro.com>
2023-05-31 11:01:48 +0100

    set up product route controllers

---
+++ src/server/controllers/prooductController.ts
@@ -0,0 +1,29 @@
+import Express from "express";
+import { asyncHandler } from "../middleware/asyncHandler";
+import Product from "../models/productModel";
+
+// @desc Fetch all Products
+// @route GET "/api/products"
+// @access Public
+export const getProducts = asyncHandler(
+  async (req: Express.Request, res: Express.Response) => {
+    const products = await Product.find({});
+    res.json(products);
+  }
+);
+
+// @desc Fetch single Product
+// @route GET "/api/products/:id"
+// @access Public
+export const getProductById = asyncHandler(
+  async (req: Express.Request, res: Express.Response) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Resource not found");
+    }
+  }
+);
--- src/server/routes/productRoutes.ts
+++ src/server/routes/productRoutes.ts
@@ -1,8 +1,6 @@
 import Express from "express";
 // import { Send, Query } from "express-serve-static-core";
-import Product from "../models/productModel";
-import mongoose from "mongoose";
-import { asyncHandler } from "../middleware/asyncHandler";
+import { getProductById, getProducts } from "../controllers/prooductController";
 
 const router = Express.Router();
 
@@ -23,36 +21,8 @@ const router = Express.Router();
 //   json: Send<ResBody, this>;
 // }
 
-// @desc Fetch all Products
-// @route GET "/api/products"
-// @access Public
-router.get(
-  "/",
-  asyncHandler(async (_req: Express.Request, res: Express.Response) => {
-    const products = await Product.find({});
-    console.log("Request made to /api/products");
-    res.json(products);
-  })
-);
+router.route("/").get(getProducts);
 
-// @desc Fetch single Products
-// @route GET "/api/products/:id"
-// @access Public
-router.get(
-  "/:id",
-  asyncHandler(async (req: Express.Request, res: Express.Response) => {
-    if (mongoose.Types.ObjectId.isValid(req.params.id)) {
-      const product = await Product.findById(req.params.id);
-
-      if (product) {
-        res.json(product);
-      } else {
-        res.status(404).json({ message: "Product not found" });
-      }
-    } else {
-      res.status(404).json({ message: "Product not found" });
-    }
-  })
-);
+router.route("/:id").get(getProductById);
 
 export default router;
