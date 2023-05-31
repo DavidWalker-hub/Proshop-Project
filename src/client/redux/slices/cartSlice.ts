7330b90f7410ba2c31ddc72f91e00337051a2c18
David Walker <david@skibro.com>
2023-05-31 18:11:13 +0100

    improve naming and complete price calculation for cart

--- src/client/redux/slices/cartSlice.ts
+++ src/client/redux/slices/cartSlice.ts
@@ -1,6 +1,10 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { Item } from "../../../types/cart";
 
+const addDecimals = (num: number) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
+
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart") as string)
   : { cartItems: [] as Item[] };
@@ -10,18 +14,43 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action: PayloadAction<Item>) => {
-      const item = action.payload;
+      const newItem = action.payload;
 
-      const existItem = state.cartItems.find((i: Item) => i._id === item._id);
+      const existItem = state.cartItems.find(
+        (item: Item) => item._id === newItem._id
+      );
       if (existItem) {
-        state.cartItems = state.cartItems.map((i: Item) =>
-          i._id === existItem._id ? item : i
+        state.cartItems = state.cartItems.map((item: Item) =>
+          item._id === existItem._id ? newItem : item
         );
       } else {
-        state.cartItems = [...state.cartItems, item];
+        state.cartItems = [...state.cartItems, newItem];
       }
+
+      //   calculate items price
+      state.itemsPrice = addDecimals(
+        state.cartItems.reduce(
+          (acc: number, item: Item) => acc + item.price * item.qty,
+          0
+        )
+      );
+      // calculate shipping price if order over £100 free, else £10
+      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+      // calculate tax price (15%)
+      state.taxPrice = addDecimals(
+        Number((0.15 * state.itemsPrice).toFixed(2))
+      );
+      // calculate total price
+      state.totalPrice = (
+        Number(state.itemsPrice) +
+        Number(state.shippingPrice) +
+        Number(state.taxPrice)
+      ).toFixed(2);
+
+      localStorage.setItem("cart", JSON.stringify(state));
     },
   },
 });
 
+export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
--- src/types/cart.ts
+++ src/types/cart.ts
@@ -1,4 +1,7 @@
 export interface Item {
   _id: string;
   name: string;
+  qty: number;
+  image: string;
+  price: number;
 }
