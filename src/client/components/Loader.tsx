a2a4098d0ba76aabbfe3f6c5c079472f3b866378
David Walker <david@skibro.com>
2023-05-31 14:25:04 +0100

    create a spinner

---
+++ src/client/components/Loader.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../assets/styles/loader.css";
+
+export function Loader() {
+  return (
+    <div className="spinnerContainer">
+      <div className="spinner"></div>
+    </div>
+  );
+}
