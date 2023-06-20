d56e57382ae434af353aeb94fc9761a4fb5e2740
David Walker <david_walker1@hotmail.co.uk>
2023-06-20 18:21:16 +0100

    create userApiSlice with first action of login

---
+++ src/client/redux/slices/usersApiSlice.ts
@@ -0,0 +1,17 @@
+import { User } from "../../../types/user";
+import { USERS_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation<User, Partial<User> & Pick<User, "_id">>({
+      query: (data) => ({
+        url: `${USERS_URL}/auth`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation } = usersApiSlice;
