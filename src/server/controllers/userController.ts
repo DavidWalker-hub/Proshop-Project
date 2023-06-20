3df0be08d5e0a8ded3549fc70315c37c4721baab
David Walker <david_walker1@hotmail.co.uk>
2023-06-20 16:28:16 +0100

    create register route and user profile GET and PUT routes

--- src/server/controllers/userController.ts
+++ src/server/controllers/userController.ts
@@ -1,7 +1,7 @@
 import Express from "express";
 import { asyncHandler } from "../middleware/asyncHandler";
 import User from "../models/userModel";
-import jwt, { Secret } from "jsonwebtoken";
+import { generateToken } from "../utils/generateToken";
 
 // @desc Auth user & get token
 // @route POST /api/users/login
@@ -12,23 +12,9 @@ export const loginUser = asyncHandler(
     const user = await User.findOne({ email });
 
     if (user && (await user.matchPassword(password))) {
-      const token = jwt.sign(
-        { userId: user._id },
-        process.env.JWT_SECRET as Secret,
-        {
-          expiresIn: "30d",
-        }
-      );
+      generateToken(res, user._id);
 
-      //   set JWT as HTTP-Only cookie
-      res.cookie("jwt", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== "development",
-        sameSite: "strict",
-        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
-      });
-
-      res.json({
+      res.status(200).json({
         _id: user._id,
         name: user.name,
         email: user.email,
@@ -46,7 +32,33 @@ export const loginUser = asyncHandler(
 // Public
 export const registerUser = asyncHandler(
   async (req: Express.Request, res: Express.Response) => {
-    res.send("register user");
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      res.status(400);
+      throw new Error("User already exists");
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    if (user) {
+      generateToken(res, user._id);
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      });
+    } else {
+      res.status(400);
+      throw new Error("Invalid user data");
+    }
   }
 );
 
@@ -65,8 +77,18 @@ export const logoutUser = asyncHandler(
 // private
 export const getUserProfile = asyncHandler(
   async (req: Express.Request, res: Express.Response) => {
-    res.json(req.user);
-    res.send("get user profile");
+    const user = await User.findById(req.user?._id);
+    if (user) {
+      res.status(200).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      });
+    } else {
+      res.status(404);
+      throw new Error("User not found");
+    }
   }
 );
 
@@ -75,7 +97,27 @@ export const getUserProfile = asyncHandler(
 // private
 export const updateUserProfile = asyncHandler(
   async (req: Express.Request, res: Express.Response) => {
-    res.send("update user profile");
+    const user = await User.findById(req.user?._id);
+
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      if (req.body.password) {
+        user.password = req.body.password;
+      }
+
+      const updatedUser = await user.save();
+
+      res.status(200).json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+      });
+    } else {
+      res.status(404);
+      throw new Error("User not found");
+    }
   }
 );
 
