73b5afa9791b5c77bfee778829ff6b84218f3692
David Walker <david_walker1@hotmail.co.uk>
2023-06-21 18:15:40 +0100

    fix login route and mutation typings

--- src/client/redux/slices/usersApiSlice.ts
+++ src/client/redux/slices/usersApiSlice.ts
@@ -4,9 +4,12 @@ import { apiSlice } from "./apiSlice";
 
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    login: builder.mutation<User, Partial<User> & Pick<User, "_id">>({
+    login: builder.mutation<
+      User,
+      Partial<User> & Pick<User, "email" | "password">
+    >({
       query: (data) => ({
-        url: `${USERS_URL}/auth`,
+        url: `${USERS_URL}/login`,
         method: "POST",
         body: data,
       }),
